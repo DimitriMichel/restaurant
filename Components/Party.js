134cf8035717f1ef3106f4edef5a879493edcfc9
Dimitri <dimitrimachines@gmail.com>
2020-12-16 03:18:46 -0500

    new party background image

---
+++ Components/Party.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Party.module.css';
+import Image from 'next/image';
+
+const Party = () => {
+  return (
+    <div className={styles.information}>
+      <div className={styles.iceCreamImage}>
+      </div>
+    </div>
+  );
+};
+
+export default Party;
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,13 @@ import styles from './index.module.css';
 import Hero from '../Components/Hero';
 import Social from '../Components/Social';
 import Info from '../Components/Info';
+import Party from '../Components/Party';
 const Home = () => {
   return (
     <div className={styles.container}>
       <Hero />
       <Info/>
+      <Party/>
     </div>
   );
 };
